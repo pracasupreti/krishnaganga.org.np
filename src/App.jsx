2c24e8d90394df3509c5ef85baa49d401de7a3e4
Abhay Kumar Gupta <akgupta128125@gmail.com>
2025-03-26 22:38:18 +0545

    vite install

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h2 className='text-red-600 text-center'>hello my name is abahy </h2>
+    </>
+  )
+}
+
+export default App
