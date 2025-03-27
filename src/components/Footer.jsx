3bb406dcda334bae9603e3586fef1c65f8e4cfba
Abhay Kumar Gupta <akgupta128125@gmail.com>
2025-03-27 20:51:21 +0545

    header&footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 function Footer() {
   return (
     <>
-      
+      <footer className='bg-[#32302f] p-4'>
+        <section className='container mx-auto lg:flex justify-between text-[#8f8b89] text-sm'>
+          <div className='flex-col gap-4 flex'>
+            <h2 className='text-[#ecc731] text-[28px] font-bold'><em>About Us</em></h2>
+            <p>Situated in the heart city of Biratnagar, HOPE TRUST, Krishna Ganga Drug & Alcohol Deaddiction & Rehabilitation Center</p>
+            <div className='flex flex-col gap-2'>
+              <p className='flex gap-2'><img className='bg-[#8f8b89] rounded h-[20px] w-[20px]' src="src/assets/icon/home.svg" alt="home" />Janapath Tole, Biratnagar, Nepal</p>
+              <a href='tel:+977 21471609' className='flex gap-2 hover:text-[#ccc]'><img className='bg-[#8f8b89] rounded h-[20px] w-[20px]' src="src/assets/icon/phone.svg" alt="phone" />+977 21 471609</a>
+              <a href='mailto:krishnaganga@gmail.com' className='flex gap-2 hover:text-[#ccc]'><img className='bg-[#8f8b89] rounded h-[20px] w-[20px]' src="src/assets/icon/envelope.svg" alt="envelope" />krishnaganga@gmail.com</a>
+            </div>
+          </div>
+          <div>
+            <h3 className='text-white text-[24px] mb-5'>Useful Links</h3>
+            <div className='flex flex-col gap-2'>
+            <a className='hover:text-[#ccc]' href="https://neurohospital.com.np/" target='_blank'>Neuro Hospital,Biratnagar</a>
+            <a className='hover:text-[#ccc]' href="https://www.swc.org.np/" target='_blank'>Social Welfare Council</a>
+            <a className='hover:text-[#ccc]' href="https://ncb.nepalpolice.gov.np/" target='_blank'>Narcotics Control Bureau</a>
+            <a className='hover:text-[#ccc]' href="https://www.mofa.gov.np/" target='_blank'>Ministry of Foreign Affairs</a>
+            </div>
+          </div>
+        </section>
+      </footer>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,31 +4,36 @@ function Header() {
   return (
     <>
       <section className='bg-[#26211d]'>
-        <section>
-          {/* <ul>
-            <li><img src="src\assets\icon\facebook.svg" alt="facebook" /></li>
-            <li><img src="src\assets\icon\google-plus.svg" alt="google-plus" /></li>
-            <li><img src="src\assets\icon\twitter.svg" alt="twitter" /></li>
-            <li><img src="src\assets\icon\linkedin.svg" alt="linkedin" /></li>
-            <li><img src="src\assets\icon\pinterest.svg" alt="pinterest" /></li>
-          </ul> */}
+        <section className='container mx-auto flex justify-between p-2'>
+          <ul className='flex gap-2'>
+            <li><img className='w-[25px] h-[25px]' src="src\assets\icon\facebook.svg" alt="facebook" /></li>
+            <li><img className='w-[25px] h-[25px]' src="src\assets\icon\google-plus.svg" alt="google-plus" /></li>
+            <li><img className='w-[25px] h-[25px]' src="src\assets\icon\twitter.svg" alt="twitter" /></li>
+            <li><img className='w-[25px] h-[25px]' src="src\assets\icon\linkedin.svg" alt="linkedin" /></li>
+            <li><img className='w-[25px] h-[25px]' src="src\assets\icon\pinterest.svg" alt="pinterest" /></li>
+          </ul>
+          <div className='uppercase text-white'>
+            <a href="#" className='border px-4 py-2 hover:bg-[#ecc731] me-2'>Donate us</a>
+            <a href="#" className='border px-4 py-2 hover:bg-[#ecc731]'>Volunteer</a>
+          </div>
         </section>
-        <header className='bg-[#ecc731] text-white p-2'>
-          <section className='container mx-auto flex items-center justify-content-between'>
+        <header className='bg-[#ecc731] p-2'>
+          <section className='container mx-auto flex items-center justify-between'>
           <div>
             <img src="src/assets/logo/krishna-ganga-logo.png" alt="Krishna Ganga Drugs & Alcohol Deaddiction & Rehabilitation Center" />
           </div>
-          <ul className='flex gap-4 uppercase'>
-            <li>home</li>
-            <li>about</li>
-            <li>treatment</li>
-            <li>fee</li>
-            <li>banned items</li>
-            <li>duration</li>
-            <li>contact</li>
+          <ul className='flex gap-4 uppercase text-black font-medium text-[16px]'>
+            <li className='hover:font-bold'>home</li>
+            <li  className='hover:font-bold'>about</li>
+            <li  className='hover:font-bold'>treatment</li>
+            <li  className='hover:font-bold'>fee</li>
+            <li  className='hover:font-bold'>banned items</li>
+            <li  className='hover:font-bold'>duration</li>
+            <li  className='hover:font-bold'>contact</li>
           </ul>
-          <form action="#">
-            <input type="text" className='' />
+          <form action="#" className='flex'>
+            <input type="text" className='p-2 w-[300px] outline-none border bg-[#ecc731]' />
+            <img src="src/assets/icon/search-icon.svg" alt="search-icon" className='h-[25px] w-[25px] relative top-2 left-[-30px]'/>
           </form>
           </section>
        
