5342ca504fe79b27df519418d05aa8f0d623ea76
Abhay Kumar Gupta <akgupta128125@gmail.com>
2025-04-11 03:12:30 +0545

    volunteer form

--- src/pages/Volunteer.jsx
+++ src/pages/Volunteer.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 function Volunteer() {
+  const [currentDate, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    const date = new Date().toISOString().split('T')[0];
+    setCurrentDate(date); // ✅ use the setCurrentDate from useState
+  }, []);
+  
+ 
+
   return (
     <>
       <section className='container mx-auto my-8'>
@@ -12,7 +22,7 @@ function Volunteer() {
           <form className='lg:w-[50%] md:w-[60%] w-[80%] shadow-lg px-4 py-2 mx-auto flex flex-col gap-4'>
             <label htmlFor="name">
               <p>Full Name :</p>
-              <input type="text" className='name capitalize border px-4 py-1 w-full outline-none mb-2' id='name' />
+              <input type="text" name='name' className='name capitalize border px-4 py-1 w-full outline-none mb-2' id='name' />
             </label>
             <div className='flex flex-wrap gap-4 gap-y-0'>
               <p>Gender : </p>
@@ -31,7 +41,7 @@ function Volunteer() {
             </label>
             <label htmlFor='number'><p>Phone Number : </p>
               <div className='flex gap-2'>
-                <select className='border px-4 py-1 outline-none w-[50%]' onChange={(e) => { console.log('Selected Code:', e.target.value); }} name='countryCode' defaultValue='+1'>
+                <select className='border px-4 py-1 outline-none w-1/2' onChange={(e) => { console.log('Selected Code:', e.target.value); }} name='countryCode' defaultValue='+1'>
                   <option value='+61'>🇦🇺 +61 (Australia)</option>
                   <option value='+55'>🇧🇷 +55 (Brazil)</option>
                   <option value='+86'>🇨🇳 +86 (China)</option>
@@ -75,101 +85,102 @@ function Volunteer() {
             </label>
             <label htmlFor="availability">
               <p>Availability</p>
+              <div className='flex flex-col gap-2'>
+                <div className='flex gap-6 gap-y-0 flex-wrap'>
+                  <span>Monday : </span>
+                  <div>
+                    <input type="checkbox" name="monday" value="7AM-10AM" /> (7AM-10AM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="monday" value="10AM-4PM" /> (10AM-4PM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="monday" value="4PM-11PM" /> (4PM-11PM)
+                  </div>
+                </div>
 
-              <div className='flex gap-6'>
-                <span>Monday : </span>
-                <div>
-                  <input type="checkbox" name="monday" value="7AM-10AM" /> (7AM-10AM)
+                <div className='flex gap-6 gap-y-0 flex-wrap'>
+                  <span>Tuesday : </span>
+                  <div>
+                    <input type="checkbox" name="tuesday" value="7AM-10AM" /> (7AM-10AM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="tuesday" value="10AM-4PM" /> (10AM-4PM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="tuesday" value="4PM-11PM" /> (4PM-11PM)
+                  </div>
                 </div>
-                <div>
-                  <input type="checkbox" name="monday" value="10AM-4PM" /> (10AM-4PM)
-                </div>
-                <div>
-                  <input type="checkbox" name="monday" value="4PM-11PM" /> (4PM-11PM)
-                </div>
-              </div>
 
-              <div className='flex gap-6'>
-                <span>Tuesday : </span>
-                <div>
-                  <input type="checkbox" name="tuesday" value="7AM-10AM" /> (7AM-10AM)
+                <div className='flex gap-6 gap-y-0 flex-wrap'>
+                  <span>Wednesday : </span>
+                  <div>
+                    <input type="checkbox" name="wednesday" value="7AM-10AM" /> (7AM-10AM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="wednesday" value="10AM-4PM" /> (10AM-4PM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="wednesday" value="4PM-11PM" /> (4PM-11PM)
+                  </div>
                 </div>
-                <div>
-                  <input type="checkbox" name="tuesday" value="10AM-4PM" /> (10AM-4PM)
-                </div>
-                <div>
-                  <input type="checkbox" name="tuesday" value="4PM-11PM" /> (4PM-11PM)
-                </div>
-              </div>
 
-              <div className='flex gap-6'>
-                <span>Wednesday : </span>
-                <div>
-                  <input type="checkbox" name="wednesday" value="7AM-10AM" /> (7AM-10AM)
+                <div className='flex gap-6 gap-y-0 flex-wrap'>
+                  <span>Thursday : </span>
+                  <div>
+                    <input type="checkbox" name="thursday" value="7AM-10AM" /> (7AM-10AM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="thursday" value="10AM-4PM" /> (10AM-4PM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="thursday" value="4PM-11PM" /> (4PM-11PM)
+                  </div>
                 </div>
-                <div>
-                  <input type="checkbox" name="wednesday" value="10AM-4PM" /> (10AM-4PM)
-                </div>
-                <div>
-                  <input type="checkbox" name="wednesday" value="4PM-11PM" /> (4PM-11PM)
-                </div>
-              </div>
 
-              <div className='flex gap-6'>
-                <span>Thursday : </span>
-                <div>
-                  <input type="checkbox" name="thursday" value="7AM-10AM" /> (7AM-10AM)
+                <div className='flex gap-6 gap-y-0 flex-wrap'>
+                  <span>Friday : </span>
+                  <div>
+                    <input type="checkbox" name="friday" value="7AM-10AM" /> (7AM-10AM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="friday" value="10AM-4PM" /> (10AM-4PM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="friday" value="4PM-11PM" /> (4PM-11PM)
+                  </div>
                 </div>
-                <div>
-                  <input type="checkbox" name="thursday" value="10AM-4PM" /> (10AM-4PM)
-                </div>
-                <div>
-                  <input type="checkbox" name="thursday" value="4PM-11PM" /> (4PM-11PM)
-                </div>
-              </div>
 
-              <div className='flex gap-6'>
-                <span>Friday : </span>
-                <div>
-                  <input type="checkbox" name="friday" value="7AM-10AM" /> (7AM-10AM)
+                <div className='flex gap-6 gap-y-0 flex-wrap'>
+                  <span>Saturday : </span>
+                  <div>
+                    <input type="checkbox" name="saturday" value="7AM-10AM" /> (7AM-10AM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="saturday" value="10AM-4PM" /> (10AM-4PM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="saturday" value="4PM-11PM" /> (4PM-11PM)
+                  </div>
                 </div>
-                <div>
-                  <input type="checkbox" name="friday" value="10AM-4PM" /> (10AM-4PM)
-                </div>
-                <div>
-                  <input type="checkbox" name="friday" value="4PM-11PM" /> (4PM-11PM)
-                </div>
-              </div>
 
-              <div className='flex gap-6'>
-                <span>Saturday : </span>
-                <div>
-                  <input type="checkbox" name="saturday" value="7AM-10AM" /> (7AM-10AM)
-                </div>
-                <div>
-                  <input type="checkbox" name="saturday" value="10AM-4PM" /> (10AM-4PM)
-                </div>
-                <div>
-                  <input type="checkbox" name="saturday" value="4PM-11PM" /> (4PM-11PM)
-                </div>
-              </div>
-
-              <div className='flex gap-6'>
-                <span>Sunday : </span>
-                <div>
-                  <input type="checkbox" name="sunday" value="7AM-10AM" /> (7AM-10AM)
-                </div>
-                <div>
-                  <input type="checkbox" name="sunday" value="10AM-4PM" /> (10AM-4PM)
-                </div>
-                <div>
-                  <input type="checkbox" name="sunday" value="4PM-11PM" /> (4PM-11PM)
+                <div className='flex gap-6 gap-y-0 flex-wrap'>
+                  <span>Sunday : </span>
+                  <div>
+                    <input type="checkbox" name="sunday" value="7AM-10AM" /> (7AM-10AM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="sunday" value="10AM-4PM" /> (10AM-4PM)
+                  </div>
+                  <div>
+                    <input type="checkbox" name="sunday" value="4PM-11PM" /> (4PM-11PM)
+                  </div>
                 </div>
               </div>
             </label>
             <label htmlFor="area">
               <p>Preferred Area of Volunteering</p>
-              <select className='w-full border outline-none px-4 py-1' name="area" id="area" onChange={(e) => {
+              <select className='w-full border outline-none px-4 py-1' name="volunteer_area" id="area" onChange={(e) => {
                 const inputField = document.getElementById('otherAreaInput');
                 if (e.target.value === 'others') {
                   inputField.style.display = 'block'; // Show the input field when 'Others' is selected
@@ -187,12 +198,12 @@ function Volunteer() {
                 <option value="Medical Support (for professionals)">Medical Support (for professionals)</option>
                 <option value="others">Others</option>
               </select>
-              <input type="text" id="otherAreaInput" className='outline-none border px-4 py-1 w-full' style={{display:'none'}}  placeholder="Please specify"/>
+              <input type="text" id="otherAreaInput" className='outline-none border px-4 py-1 w-full' style={{ display: 'none' }} placeholder="Please specify" />
             </label>
 
             <label htmlFor="area">
               <p>Do you have any previous volunteering experience?</p>
-              <select className='w-full border outline-none px-4 py-1' defaultChecked='0' name="area" id="area" onChange={(e) => {
+              <select className='w-full border outline-none px-4 py-1' defaultChecked='0' name="volunteer_experience" id="area" onChange={(e) => {
                 const inputField = document.getElementById('yes');
                 if (e.target.value === 'yes') {
                   inputField.style.display = 'block'; // Show the input field when 'Others' is selected
@@ -203,19 +214,137 @@ function Volunteer() {
                 <option value="" disabled selected>Select an option</option>
                 <option value="yes">Yes</option>
                 <option value="no">No</option>
-
-                
               </select>
-              <input type="text" id="yes" className='outline-none border px-4 py-1 w-full' style={{display:'none'}}  placeholder="Please specify"/>
+              <input type="text" id="yes" className='outline-none border px-4 py-1 w-full' style={{ display: 'none' }} placeholder="Please specify" />
             </label>
             <label htmlFor="reason">
               <p>Why do you want to volunteer with us?</p>
               <textarea name="reason_volunteer" id="reason" className='w-full border outline-none px-4 py-1'></textarea>
             </label>
+
+
             <label htmlFor="skill">
               <p>Do you have any relevant qualifications, skills, or training?</p>
-              <input type="text" id='skill' name='skill' className='border outline-none px-4 py-1 w-full'/>
+              <select className='w-full border outline-none px-4 py-1' defaultChecked='0' name="skill" id="skill" onChange={(e) => {
+                const inputField = document.getElementById('yes1');
+                if (e.target.value === 'yes1') {
+                  inputField.style.display = 'block'; // Show the input field when 'Others' is selected
+                } else {
+                  inputField.style.display = 'none'; // Hide it when another option is selected
+                }
+              }}>
+                <option value="" disabled selected>Select an option</option>
+                <option value="yes1">Yes</option>
+                <option value="no">No</option>
+              </select>
+              <input type="text" id="yes1" className='outline-none border px-4 py-1 w-full' style={{ display: 'none' }} placeholder="Please specify" />
             </label>
+
+            <label htmlFor="addiction">
+              <p>Do you have any medical conditions we should be aware of?</p>
+              <select className='w-full border outline-none px-4 py-1' defaultChecked='0' name="addiction" id="addiction" onChange={(e) => {
+                const inputField = document.getElementById('yes2');
+                if (e.target.value === 'yes2') {
+                  inputField.style.display = 'block'; // Show the input field when 'Others' is selected
+                } else {
+                  inputField.style.display = 'none'; // Hide it when another option is selected
+                }
+              }}>
+                <option value="" disabled selected>Select an option</option>
+                <option value="yes2">Yes</option>
+                <option value="no">No</option>
+              </select>
+              <input type="text" id="yes2" className='outline-none border px-4 py-1 w-full' style={{ display: 'none' }} placeholder="Please specify" />
+            </label>
+
+            <label htmlFor="recovery">
+              <p>Are you in recovery yourself?</p>
+              <select className='w-full border outline-none px-4 py-1' defaultChecked='0' name="recovery_stage" id="recovery" onChange={(e) => {
+                const inputField = document.getElementById('yes3');
+                if (e.target.value === 'yes3') {
+                  inputField.style.display = 'block'; // Show the input field when 'Others' is selected
+                } else {
+                  inputField.style.display = 'none'; // Hide it when another option is selected
+                }
+              }}>
+                <option value="" disabled selected>Select an option</option>
+                <option value="yes3">Yes</option>
+                <option value="no">No</option>
+              </select>
+              <input type="text" id="yes3" className='outline-none border px-4 py-1 w-full' style={{ display: 'none' }} placeholder="Please specify" />
+            </label>
+
+            <label htmlFor="recovery">
+              <p>Have you ever worked in a rehab or health-related setting before?</p>
+              <select className='w-full border outline-none px-4 py-1' defaultChecked='0' name="work_experience" id="recovery" onChange={(e) => {
+                const inputField = document.getElementById('yes4');
+                if (e.target.value === 'yes4') {
+                  inputField.style.display = 'block'; // Show the input field when 'Others' is selected
+                } else {
+                  inputField.style.display = 'none'; // Hide it when another option is selected
+                }
+              }}>
+                <option value="" disabled selected>Select an option</option>
+                <option value="yes4">Yes</option>
+                <option value="no">No</option>
+              </select>
+              <input type="text" id="yes4" className='outline-none border px-4 py-1 w-full' style={{ display: 'none' }} placeholder="Please specify" />
+            </label>
+
+            <label htmlFor="e_name">
+              <p>Emergency Contact Name:</p>
+              <input type="text" id='e_name' className='border outline-none w-full px-4 py-1'/>
+            </label>
+
+            <label htmlFor='e_number'><p>Phone Number : </p>
+              <div className='flex gap-2'>
+                <select className='border px-4 py-1 outline-none w-[50%]' onChange={(e) => { console.log('Selected Code:', e.target.value); }} name='countryCode' defaultValue='+1'>
+                  <option value='+61'>🇦🇺 +61 (Australia)</option>
+                  <option value='+55'>🇧🇷 +55 (Brazil)</option>
+                  <option value='+86'>🇨🇳 +86 (China)</option>
+                  <option value='+20'>🇪🇬 +20 (Egypt)</option>
+                  <option value='+33'>🇫🇷 +33 (France)</option>
+                  <option value='+49'>🇩🇪 +49 (Germany)</option>
+                  <option value='+91'>🇮🇳 +91 (India)</option>
+                  <option value='+62'>🇮🇩 +62 (Indonesia)</option>
+                  <option value='+81'>🇯🇵 +81 (Japan)</option>
+                  <option value='+60'>🇲🇾 +60 (Malaysia)</option>
+                  <option value='+977'>🇳🇵 +977 (Nepal)</option>
+                  <option value='+64'>🇳🇿 +64 (New Zealand)</option>
+                  <option value='+92'>🇵🇰 +92 (Pakistan)</option>
+                  <option value='+63'>🇵🇭 +63 (Philippines)</option>
+                  <option value='+7'>🇷🇺 +7 (Russia)</option>
+                  <option value='+65'>🇸🇬 +65 (Singapore)</option>
+                  <option value='+82'>🇰🇷 +82 (South Korea)</option>
+                  <option value='+34'>🇪🇸 +34 (Spain)</option>
+                  <option value='+44'>🇬🇧 +44 (UK)</option>
+                  <option value='+1'>🇺🇸 +1 (USA)</option>
+                  <option value='+39'>🇮🇹 +39 (Italy)</option>
+
+                  {/* Add more or generate from a list */}
+                </select>
+                <input type='text' maxLength={10} pattern='[0-9]*' inputMode='numeric' name="e_number" id="e_number" className='border w-full px-4 py-1 outline-none' onInput={(e) => {
+                  e.target.value = e.target.value.replace(/[^0-9]/g, '').slice(0, 10);
+                }} />
+              </div>
+            </label>
+
+            <label htmlFor="relation">
+              <p>Relation : </p>
+              <input type="text" name='relation' className='border outline-none w-full px-4 py-1' />
+            </label>
+
+            <label htmlFor="policy">
+              <input type="checkbox" name="policy" id="policy" className='w-4 h-4' />&nbsp; I agree to follow the center’s rules and confidentiality policy
+            </label>
+
+            <label htmlFor="application date">
+              <input type="date" value={currentDate} name="application_date" id="application_date" className='border w-full px-4 py-2 outline-none' />
+            </label>
+            <div className='flex gap-4'>
+            <button type='submit' className='border outline-none px-4 py-1 hover:shadow-lg '>Submit</button>
+            <button type='clear' className='border outline-none px-4 py-1 hover:shadow-lg'><Link to={'/volunteer'}>Clear</Link></button>
+            </div>
           </form>
         </div>
       </section>
